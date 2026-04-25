5f8021ccdb5ea199902bc80b4bcce45cba668acd
pedro-santos15 <pedrohssantos1509@gmail.com>
2026-04-25 17:16:20 -0300

    feat: add an TransacaoModel ArrayList in the entity class

--- src/main/java/com/pedrosantos15/desafioItau/model/TransacaoModel.java
+++ src/main/java/com/pedrosantos15/desafioItau/model/TransacaoModel.java
@@ -2,12 +2,15 @@ package com.pedrosantos15.desafioItau.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,4 +20,8 @@ import java.time.OffsetDateTime;
 public class TransacaoModel {
     private BigDecimal valor;
     private OffsetDateTime dataHora;
+
+    @Getter
+    private static List<TransacaoModel> transacoes = new ArrayList<>();
+
 }
